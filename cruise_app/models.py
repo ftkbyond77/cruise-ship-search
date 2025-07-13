a303f3b6d5f5c4243f3a83e12f1ef622669d64da
Chayut Meekhun <chayutmeekhun4@gmail.com>
2025-07-13 16:14:55 +0700

    before migration

--- cruise_app/models.py
+++ cruise_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class CrewImage(models.Model):
+    IMAGE_TYPES = [
+        ('ID1', 'ID2'),
+    ]
+
+    crew_id_number = models.CharField(max_length=20)  
+    image_type = models.CharField(max_length=20, choices=IMAGE_TYPES)
+    image = models.ImageField(upload_to='uploads/')  
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.crew_id_number} - {self.image_type}"
--- cruise_app/views.py
+++ cruise_app/views.py
@@ -1,15 +1,12 @@
-from django.shortcuts import render
-from .mongo.mongo import db  
-from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
+from .mongo.mongo import db
 
-
-def person_list(request):
+def home(request):
     persons = list(db.persons.find())
-    return render(request, 'person_list.html', {'persons': persons})
+    return render(request, 'home.html', {'persons': persons})
 
 def person_detail(request, crew_id):
-    person = db.persons.find_one({"crew_id_number": crew_id})
-    return render(request, 'person_detail.html', {'person': person})
-
-def home_redirect(request):
-    return redirect('person_list')
+    person = db.persons.find_one({'crew_id_number': crew_id})
+    if not person:
+        return render(request, '404.html', status=404)
+    return render(request, 'detail.html', {'person': person})
