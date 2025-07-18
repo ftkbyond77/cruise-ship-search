39c1e36b8f702a3655413e62f693c6d39317f722
Chayut Meekhun <chayutmeekhun4@gmail.com>
2025-07-18 08:57:04 +0700

    urls + home

--- cruise_app/urls.py
+++ cruise_app/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('person/<str:crew_id>/', views.person_detail, name='person_detail'),
     path('image/<int:image_id>/update/', views.update_image, name='update_image'),
     path('image/<int:image_id>/delete/', views.delete_image, name='delete_image'),
-    path('debug-mongo/', views.test_mongo)
-
+    path('debug-mongo/', views.test_mongo, name='mongo_debug'),
 ]
